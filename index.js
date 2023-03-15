a09c56c6f662d9aefefd5459a48b138345d0530b
Tareq Harh <tareq310harh@gmail.com>
2023-03-15 22:43:33 +0300

    finished the 4 challenges

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+/* 1. Create a variable called multiply set to an equation that 
+will multiply the variables num1 and num2; the result of the multiplication should be 62.*/
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2
+console.log(multiply)
 
-// Write your code here
+/*2. Create a variable called random that will generate a random integer greater than 0.*/
+const random = Math.round(Math.random()*10)
+console.log(random)
+
+/*3. Create a variable called mod set to an equation that will calculate the 
+remainder of dividing variable num3 by num4; the remainder should be 4.*/
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4 
+console.log(mod)
+
+/*4. Create a variable called max that finds the highest number in a set; the value returned 
+should be 20.*/
+
+let max = Math.max(20,3.15,7,-30,14,0,-0)
+console.log(max)
